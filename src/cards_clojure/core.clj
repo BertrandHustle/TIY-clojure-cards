1eb994a8fdc1c4cfc97ba76f5f707a168b2bb0d6
BertrandHustle <scott.greenberg926@gmail.com>
2016-06-07 17:55:02 -0400

    more readable

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -3,7 +3,7 @@
 (def suits [:clubs :spades :hearts :diamonds])
 (def ranks (range 1 14))
 
-;clean this mess up
+;"helper" functions
 (defn ascending-count [num]
   (conj '()
         (inc(inc(inc num)))
@@ -11,6 +11,13 @@
         (inc num)
         num))
 
+(defn drop-first-n [n hand]
+  (drop n (sort (map :rank hand))))
+
+(defn drop-last-n [n hand]
+  (drop-last n (sort (map :rank hand))))
+
+;creation functions + test hand
 (defn create-deck []
   (set
     ;loops over all ranks for each suit
@@ -19,11 +26,11 @@
           rank ranks]
       {:suit suit :rank rank})))
 
-(def test-hand #{{:suit :clubs, :rank 10}
-  {:suit :clubs, :rank 11}
-  {:suit :clubs, :rank 12}
-  {:suit :clubs, :rank 13}
- {:suit :clubs, :rank 1}})
+(def test-hand #{{:suit :hearts, :rank 3}
+  {:suit :clubs, :rank 1}
+  {:suit :hearts, :rank 2}
+  {:suit :spades, :rank 3}
+ {:suit :spades, :rank 1}})
 
 (defn create-hands [deck]
   (set
@@ -34,6 +41,11 @@
           card4 (disj deck card1 card2 card3)
           card5 (disj deck card1 card2 card3 card4)]
       #{card1 card2 card3 card4 card5})))
+
+(defn winning-hand [hand1 hand2]
+  )
+
+;poker hands
 
 (defn royal-flush? [hand]
   (and (straight-flush? hand)
@@ -50,11 +62,11 @@
 (defn full-house? [hand]
   (or
     (and
-      (apply = (drop 3 (sort (map :rank hand))))
-      (apply = (drop-last 2 (sort (map :rank hand)))))
+      (apply = (drop-first-n 3 hand))
+      (apply = (drop-last-n 2 hand)))
     (and
-      (apply = (drop 2 (sort (map :rank hand))))
-      (apply = (drop-last 3 (sort (map :rank hand)))))))
+      (apply = (drop-first-n 2 hand))
+      (apply = (drop-last-n 3 hand)))))
 
 (defn flush? [hand]
   (= 1 (count (set (map :suit hand)))))
@@ -69,19 +81,19 @@
 
 (defn three-of-a-kind? [hand]
   (or
-    (apply = (drop 2 (sort (map :rank hand))))
-    (apply = (drop-last 2 (sort (map :rank hand)))))
+    (apply = (drop-first-n 2 hand))
+    (apply = (drop-last-n 2 hand)))
   )
 
 (defn two-pair? [hand]
   (and
-    (apply = (drop 3 (sort (map :rank hand))))
-    (apply = (drop-last 3 (sort (map :rank hand))))))
+    (apply = (drop-first-n 3 hand))
+    (apply = (drop-last-n 3 hand))))
 
 (defn pair? [hand]
   (or
-    (apply = (drop 3 (sort (map :rank hand))))
-    (apply = (drop-last 3 (sort (map :rank hand))))))
+    (apply = (drop-first-n 3 hand))
+    (apply = (drop-last-n 3 hand))))
 
 (defn -main []
   (let [deck (create-deck)
